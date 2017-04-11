a8d5e63edfac3d0ccc9c203d17d7f500e46e1716
Patresss <patresss@wp.pl>
2017-04-11 21:55:01 +0200

    remove unused files

--- wzor/Sara/godzina.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python -*- coding: utf-8 -*-
-
-from datetime import datetime
-import os
-
-godzina = datetime.now().strftime('%H:%M:%S')
-
-
-stream = os.popen('/home/pi/speech.sh Aktualna godzina to ' + godzina)
